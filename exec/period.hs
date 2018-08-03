531ff67e71fd3d280cd1a6b5af3cf82cd9349f66
Alexey Karakulov <ankarakulov@gmail.com>
2018-08-03 16:38:22 +0300

    Add usage examples

--- exec/period.hs
+++ exec/period.hs
@@ -9,11 +9,23 @@ import qualified Data.Text           as T
 import qualified Data.Text.IO        as T
 import           Options.Applicative
 import           Text.Period
-
+import Text.PrettyPrint.ANSI.Leijen (string)
 import Prelude
 
 main :: IO ()
-main = execParser (info (helper <*> optParser) $ progDesc "period") >>= runCommand
+main = execParser (info (helper <*> optParser) $ progDescDoc (Just $ string desc)) >>= runCommand
+  where
+  desc = "Expand or collapse text representation of date range.\n\
+  \\n\
+  \period collapse 2018-01-01,2018-12-31\n\
+  \=> 2018\n\n\
+  \period collapse 2018-01-01,2118-12-31\n\
+  \=> 2018,2118\n\n\
+  \period expand 2018-01\n\
+  \=> 2018-01-01,2018-01-31\n\n\
+  \period expand 2018-01-07,14\n\
+  \=> 2018-01-07,2018-01-14\
+  \"
 
 data Command
   = Collapse {
@@ -36,7 +48,6 @@ parseFmt = PeriodFmt <$>
   option (T.pack <$> str) (
     short 'd' <> long "date-sep" <> value "," <>
     help "Start and end separator, by default ',' as in 'yyyy-mm-dd,yyyy-mm-dd'")
-
 
 mkCommand :: String -> String -> Parser Command -> Mod CommandFields Command
 mkCommand cmd desc parser =
